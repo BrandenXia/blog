4b348b9de9e508f405eb0333c6cf9ad0fa5a7172
BrandenXia <xxtbranden@outlook.com>
2025-11-10 07:30:30 -0800

    feat: metadata for posts

--- src/app/(app)/posts/[slug]/page.tsx
+++ src/app/(app)/posts/[slug]/page.tsx
@@ -20,6 +20,8 @@ import type { TocItem } from "remark-flexible-toc";
 
 import "./style.css";
 
+import Link from "next/link";
+
 type Frontmatter = Record<string, unknown>;
 type Scope = {
   toc?: TocItem[];
@@ -44,6 +46,23 @@ const options: MDXRemoteOptions = {
   vfileDataIntoScope: "toc",
 };
 
+const Time = ({ date }: { date: string }) => (
+  <time dateTime={date} className="inline-flex items-center">
+    <span className="i-ph-clock mr-1 size-4" />
+    {date}
+  </time>
+);
+
+const Tags = ({ tags }: { tags: string[] }) => (
+  <div className="text-secondary inline-flex space-x-2 font-mono underline">
+    {tags.map((tag) => (
+      <span className="before:content-['#']" key={tag}>
+        <Link href={`/posts?tags=${tag}`}>{tag}</Link>
+      </span>
+    ))}
+  </div>
+);
+
 const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params;
   const title = decodeURIComponent(slug);
@@ -57,8 +76,12 @@ const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
   return (
     <>
       <main className="mx-auto flex max-w-(--breakpoint-md) grow flex-col space-y-8 px-12 xl:mx-10 xl:pr-8 xl:pl-0">
-        <div>
+        <div className="mb-6 flex flex-col space-y-4">
           <h1 className="text-4xl font-bold">{meta.title}</h1>
+          <div className="flex flex-wrap items-center space-x-2.5 text-[0.95rem]">
+            <Time date={meta.date} />
+            <Tags tags={meta.tags} />
+          </div>
         </div>
         <article className="mx-0.5 text-[1.025rem] text-pretty">{content}</article>
       </main>
